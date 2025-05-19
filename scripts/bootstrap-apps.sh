3c8795f4e4c7016ba795238a44eaaddd28cfc596
sarcasticgoat <sarcasticgoat>
2025-05-18 22:41:25 -0400

    Restore some core services

--- scripts/bootstrap-apps.sh
+++ scripts/bootstrap-apps.sh
@@ -93,6 +93,8 @@ function apply_crds() {
         https://github.com/prometheus-operator/prometheus-operator/releases/download/v0.82.2/stripped-down-crds.yaml
         # renovate: datasource=github-releases depName=kubernetes-sigs/external-dns
         https://raw.githubusercontent.com/kubernetes-sigs/external-dns/refs/tags/v0.17.0/config/crd/standard/dnsendpoint.yaml
+        # renovate: datasource=github-releases depName=kubernetes-sigs/gateway-api
+        https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/experimental-install.yaml
     )
 
     for crd in "${crds[@]}"; do
